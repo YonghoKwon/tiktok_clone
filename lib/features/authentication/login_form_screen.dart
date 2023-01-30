bc241414ca2b19eefe9501322b3234e05042e628
YonghoKwon <ho2708@naver.com>
2023-01-30 21:32:27 +0900

    feature(ONBOARDING) : 5.1 Interests Screen

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
+import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
 class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
@@ -19,6 +20,11 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
     if (_formKey.currentState != null) {
       if (_formKey.currentState!.validate()) {
         _formKey.currentState!.save();
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const InterestsScreen(),
+          ),
+        );
       }
     }
   }
@@ -54,7 +60,7 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
                 ),
                 validator: (value) {
                   if (value != null && value.isEmpty) {
-                    return "Plase write your email";
+                    return "Please write your email";
                   }
                   return null;
                 },
@@ -81,7 +87,7 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
                 ),
                 validator: (value) {
                   if (value != null && value.isEmpty) {
-                    return "Plase write your password";
+                    return "Please write your password";
                   }
                   return null;
                 },
